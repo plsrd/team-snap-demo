26a7aafd2ed22e01a9fb84a37176f2dbe7702a74
RD <racheal.larimer@gmail.com>
2025-08-11 09:06:38 -0700

    add initial post query to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,22 @@
-export default function Home() {
+import Link from 'next/link.js';
+import { type SanityDocument } from 'next-sanity';
+import { client } from '@/sanity/lib/client.ts';
+
+const POSTS_QUERY = `*[_type == "post" && defined(slug.current)] {
+  _id,
+  title,
+  slug,
+  author-> {
+    name,
+    slug
+  },
+  "image": mainImage.asset,
+  publishedAt
+}`;
+
+export default async function Home() {
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
+  console.log(posts);
   return (
     <div>
       <h1>Hello World</h1>
--- sanity/schemaTypes/authorType.ts
+++ sanity/schemaTypes/authorType.ts
@@ -1,5 +1,5 @@
-import {UserIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { UserIcon } from '@sanity/icons';
+import { defineField, defineType } from 'sanity';
 
 export const authorType = defineType({
   name: 'author',
@@ -18,29 +18,10 @@ export const authorType = defineType({
         source: 'name',
       },
     }),
-    defineField({
-      name: 'image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'bio',
-      type: 'array',
-      of: [
-        defineArrayMember({
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        }),
-      ],
-    }),
   ],
   preview: {
     select: {
       title: 'name',
-      media: 'image',
     },
   },
-})
+});
